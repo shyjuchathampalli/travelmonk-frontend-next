cc8cbd2331ad78cc5b200dcdfea96e323e1c70fe
shyju-silcon <shyju@silcontechlabs.com>
2026-02-16 13:27:53 +0530

    Login page changed to force dynamic

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "@/store/store";
